86216803d5bdbdea17a7d2b60467e0460b4d3aeb
charles <charleshkang1@gmail.com>
2016-07-05 14:05:20 -0400

    finished touch events and uiresponder

---
+++ TouchTracker/TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Charles Kang on 7/5/16.
+//  Copyright © 2016 Charles Kang. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
